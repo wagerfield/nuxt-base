67d805c997148645fe3e588ee673459f414529e9
Matthew Wagerfield <matthew@wagerfield.com>
2017-11-06 21:43:42 +0000

    added message overrides

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // https://nuxtjs.org/api/internals-module-container
 module.exports = function BaseModule() {
+
+  // Messages
+  this.options.messages['error_404'] = 'Page not found'
+  this.options.messages['server_error'] = 'Server error'
+  this.options.messages['back_to_home'] = 'Back to home page'
 }
 
 module.exports.meta = require('./package.json')
