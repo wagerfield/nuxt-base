d1d04305f4923b45ce43b3806777993f058bf0fd
Matthew Wagerfield <matthew@wagerfield.com>
2017-11-06 22:26:45 +0000

    added router and css config

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // https://nuxtjs.org/api/internals-module-container
-module.exports = function BaseModule() {
+module.exports = function BaseModule(moduleOptions) {
+  const options = Object.assign({}, this.options.base, moduleOptions)
 
   // Messages
-  this.options.messages['error_404'] = 'Page not found'
-  this.options.messages['server_error'] = 'Server error'
-  this.options.messages['back_to_home'] = 'Back to home page'
+  if (options.overrideMessages !== false) {
+    Object.assign(this.options.messages, {
+      'error_404': 'Page not found',
+      'server_error': 'Server error',
+      'back_to_home': 'Back to home page'
+    }, options.messages)
+  }
+
+  // Router
+  if (options.overrideRouter !== false) {
+    Object.assign(this.options.router, {
+      linkExactActiveClass: 'active-link-exact',
+      linkActiveClass: 'active-link'
+    }, options.router)
+  }
+
+  // CSS
+  if (options.addNormalizeCSS !== false) {
+    this.options.css.push('normalize.css')
+  }
 }
 
 module.exports.meta = require('./package.json')
